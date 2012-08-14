c689701f2a0f0f6122d9d79b0302791bdcae0d0e
TrironkKiatkungwanglai <trironk@gmail.com>
2012-08-14 15:27:59 -0400

    Created Value header file and declared members of Value class.

--- MathRuntimeEvaluator/Value.h
+++ MathRuntimeEvaluator/Value.h
@@ -1,4 +1,28 @@
 #ifndef VALUE_H
 #define VALUE_H
 
+#include <string>
+
+namespace MathRuntimeEvaluator
+{
+	class Value
+	{
+		// Enumerated types of Values
+		enum Type
+		{
+			Number,
+			AssignedVariable,
+			UnassignedVariable
+		}
+		
+		// Number constructor
+		Value(double val);
+
+		// UnassignedVariable and AssignedVariable constructor
+		Value(std::string name);
+
+
+	};
+}
+
 #endif
